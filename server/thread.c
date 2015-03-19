305e12d105e4d5658f0146f43e30c2d1f77f1bfd
Michael Müller <michael@fds-team.de>
2015-03-19 01:22:34 +0100

    server: Implement support for global and local shared memory blocks based on memfd.

--- server/thread.c
+++ server/thread.c
@@ -204,6 +204,8 @@ static inline void init_thread_structure( struct thread *thread )
     thread->desktop_users   = 0;
     thread->token           = NULL;
     thread->exit_poll       = NULL;
+    thread->shm_fd          = -1;
+    thread->shm             = NULL;
     thread->esync_fd        = -1;
     thread->esync_apc_fd    = -1;
 
@@ -340,6 +342,8 @@ static void cleanup_thread( struct thread *thread )
             thread->inflight[i].client = thread->inflight[i].server = -1;
         }
     }
+    release_shared_memory( thread->shm_fd, thread->shm, sizeof(*thread->shm) );
+
     thread->req_data = NULL;
     thread->reply_data = NULL;
     thread->request_fd = NULL;
@@ -348,6 +352,9 @@ static void cleanup_thread( struct thread *thread )
     thread->context = NULL;
     thread->suspend_context = NULL;
     thread->desktop = 0;
+    thread->shm_fd = -1;
+    thread->shm = NULL;
+
 }
 
 /* destroy a thread when its refcount is 0 */
