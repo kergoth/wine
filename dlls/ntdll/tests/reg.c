15c1e58d47663a1b6a122e8232169271d854df40
Zebediah Figura <z.figura12@gmail.com>
2017-10-14 17:50:12 -0500

    ntdll: Ensure that registry keys begin with "\REGISTRY\".
    This is an anticipatory change. This part of the path should be both present
    and case-sensitive.

--- dlls/ntdll/tests/reg.c
+++ dlls/ntdll/tests/reg.c
@@ -2012,6 +2012,13 @@ static void test_RtlCreateRegistryKey(void)
     pRtlFreeUnicodeString(&str);
 }
 
+static void test_RtlFormatCurrentUserKeyPath(void)
+{
+    static const WCHAR expectedW[] = {'\\','R','E','G','I','S','T','R','Y','\\','U','S','E','R','\\'};
+
+    ok(!memcmp(winetestpath.Buffer, expectedW, sizeof(expectedW)), "String did not match\n");
+}
+
 START_TEST(reg)
 {
     static const WCHAR winetest[] = {'\\','W','i','n','e','T','e','s','t',0};
@@ -2041,6 +2048,7 @@ START_TEST(reg)
     test_NtDeleteKey();
     test_symlinks();
     test_redirection();
+    test_RtlFormatCurrentUserKeyPath();
 
     pRtlFreeUnicodeString(&winetestpath);
 
