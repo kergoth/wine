517aca7ee7402bc77de682e63a751a336f189366
Kai Krakow <kai@kaishome.de>
2018-11-26 22:06:19 +0100

    [local] dinput: Raise priority of device threads.
    This potentially reduces lag and makes the input device initialization
    faster under CPU load.
    
    Signed-off-by: Kai Krakow <kai@kaishome.de>

--- dlls/dinput/dinput_main.c
+++ dlls/dinput/dinput_main.c
@@ -1823,6 +1823,8 @@ static BOOL check_hook_thread(void)
     {
         hook_thread_event = CreateEventW(NULL, FALSE, FALSE, NULL);
         hook_thread = CreateThread(NULL, 0, hook_thread_proc, hook_thread_event, 0, &hook_thread_id);
+        if (hook_thread)
+            SetThreadPriority(hook_thread, THREAD_PRIORITY_TIME_CRITICAL);
         LeaveCriticalSection(&dinput_hook_crit);
     }
     else if (list_empty(&direct_input_list) && hook_thread)
