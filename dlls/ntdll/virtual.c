afc76649dba75e9f873a0969fd672e693f2bec95
Kai Krakow <kai@kaishome.de>
2018-12-10 01:21:55 +0100

    [local] ntdll: Add support for huge pages
    Wine already supports GetLargePageMinimum() and returns the correct page
    size. But it doesn't advise the Linux kernel to actually make use of
    this when applications use VirtualAlloc(). This commit fixes it.
    
    Signed-off-by: Kai Krakow <kai@kaishome.de>

--- dlls/ntdll/virtual.c
+++ dlls/ntdll/virtual.c
@@ -2578,6 +2578,10 @@ NTSTATUS WINAPI NtAllocateVirtualMemory( HANDLE process, PVOID *ret, ULONG zero_
         }
     }
 
+#ifdef MADV_HUGEPAGE
+    if (base && (type & MEM_LARGE_PAGES)) madvise( base, size, MADV_HUGEPAGE );
+#endif
+
     if (!status) VIRTUAL_DEBUG_DUMP_VIEW( view );
 
     if (use_locks) server_leave_uninterrupted_section( &csVirtual, &sigset );
